4fb97ba22c73a9934c0b4aa3271b4922186d2af7
Alice <alicepennec@gmail.com>
2023-12-21 10:38:42 +0100

    css added

--- src/components/tournament-card.jsx
+++ src/components/tournament-card.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router";
+import "./tournament-card.css";
 
 const TournamentCard = ({tournament}) => {
     
@@ -9,16 +10,22 @@ const TournamentCard = ({tournament}) => {
     }
 
     return ( 
-        <div onClick={() => goToTournament(tournament.id)} className="py-8 px-8 max-w-sm mx-auto bg-white rounded-wl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img className="block mx-auto h-24 sm:mx-0 sm:shrink-0" src={tournament.picture} alt="poster" />
-            <div className="text-center space-y-2 sm:text-left">
-                <div className="space-y-0.5">
-                    <h1 className="text-lg text-black font-semibold">{tournament.date}</h1>
-                    <h2>{tournament.city}</h2>
-                    <p>{tournament.name}</p>
-                    <p className="text-slate-500 font-medium"><small>{tournament.description}</small></p>
+        <div className="card">
+            <div className="container">
+                <div className="top">
+                    <div className="left">
+                        <img className="" src={tournament.picture} alt="poster" />
+                    </div>
+                    <div className="right">
+                        <h2>{tournament.date}</h2>
+                        <h3>{tournament.city}</h3>
+                        <h1>{tournament.name}</h1>
+                        <p className="">{tournament.description}</p>
+                    </div>
                 </div>
-                <button className="px-4 py-1 text-sm text-green-600 font-semibold rounded-full border border-green-200 hover:text-white hover:bg-green-600 hover:border-transparent ">Voir les détails</button>
+                <div className="bottom">
+                    <button onClick={() => goToTournament(tournament.id)} className="">Voir les détails</button>
+                </div>
             </div>
         </div>
      );
